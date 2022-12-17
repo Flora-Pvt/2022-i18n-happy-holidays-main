6ad1b03ad12369399ce09add2453df2c9ada5079
Flora Preuvot <flora@qomon.com>
2022-12-17 19:24:41 +0100

    add translation - wip

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template>
   <main class="flex flex-col justify-center h-full mx-auto max-w-600px">
     <section class="flex flex-col items-center leading-loose text-center">
-      <div class="text-3xl">
+      <div class="text-3xl mb-20">
         <span class="i-twemoji-christmas-tree"></span>
         {{ $t('happyHolidays') }}
         <span class="i-twemoji-world-map"></span>
       </div>
-      <!-- Dates - Check out locales/en.json for the key -->
-      <!-- Controls - I give you an .icon-button class if you want to use it -->
+
+      <fieldset>
+        <div
+          v-for="locale in $i18n.availableLocales"
+          :key="`locale-${locale}`"
+          class="w-full flex"
+        >
+          <input
+            v-model="$i18n.locale"
+            :value="locale"
+            type="radio"
+            id="language"
+            name="language"
+            checked
+          />
+          <label for="language">
+            <span class="mx-2" :class="flags[locale]"></span>
+            <span v-t="{ path: 'language', locale }"></span>
+          </label>
+        </div>
+      </fieldset>
+
       <!-- Flags - the current locale -->
     </section>
   </main>
@@ -16,27 +36,13 @@
 <script setup>
 import { useI18n } from 'vue-i18n'
 
+const flags = {
+  en: 'i-twemoji-flag-united-states',
+  de: 'i-twemoji-flag-germany',
+  'ja-JP': 'i-twemoji-flag-japan',
+}
 // See the README about tricky timezone issues!
 // I figured since this is i18n-friendly, we'd wanna
 // make sure the timezones were right :-)
 const christmasDate = new Date('2022/12/25')
 </script>
-
-<style scoped>
-.icon-button {
-  @apply text-xl
-    w-32px
-    h-32px
-    rounded-full
-    border-1
-    border-transparent
-    bg-transparent
-    cursor-pointer
-    duration-300
-    hover:ring-2
-    hover:border-green-500
-    hover:ring-green-500
-    hover:ring-opacity-40
-    hover:text-green-600;
-}
-</style>
