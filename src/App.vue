63c063b2235ef16053024ba12167927cb343c1d2
Flora Preuvot <flora@qomon.com>
2022-12-19 12:31:53 +0100

    add christmasIsComing feature + ts

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
   <main class="flex flex-col justify-center h-full mx-auto max-w-600px">
     <section class="flex flex-col items-center leading-loose text-center">
-      <div class="text-3xl mb-20">
-        <span class="i-twemoji-christmas-tree"></span>
+      <p class="text-3xl mb-6">
+        <i class="i-twemoji-christmas-tree" />
         {{ $t('happyHolidays') }}
-        <span class="i-twemoji-world-map"></span>
-      </div>
+        <i class="i-twemoji-world-map" />
+      </p>
+
+      <i18n-t keypath="christmasIsComing" tag="p" class="text-xl mb-16">
+        <template #date>
+          {{ $d(christmasDate, 'long') }}
+        </template>
+
+        <template #time>
+          <span class="font-medium text-green-600">
+            {{ $t('day', daysRemaining) }}
+          </span>
+        </template>
+      </i18n-t>
 
       <fieldset>
         <div
@@ -16,33 +28,32 @@
           <input
             v-model="$i18n.locale"
             :value="locale"
-            type="radio"
-            id="language"
+            :id="locale"
             name="language"
+            type="radio"
             checked
           />
-          <label for="language">
-            <span class="mx-2" :class="flags[locale]"></span>
+          <label :for="locale">
+            <i class="mx-2" :class="flags[locale]" />
             <span v-t="{ path: 'language', locale }"></span>
           </label>
         </div>
       </fieldset>
-
-      <!-- Flags - the current locale -->
     </section>
   </main>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { useI18n } from 'vue-i18n'
+
+const christmasDate = new Date('2022/12/25')
+const timeRemaining = christmasDate.getTime() - Date.now()
+const oneDay = 24 * 60 * 60 * 1000
+const daysRemaining = Math.ceil(timeRemaining / oneDay)
 
 const flags = {
   en: 'i-twemoji-flag-united-states',
   de: 'i-twemoji-flag-germany',
   'ja-JP': 'i-twemoji-flag-japan',
 }
-// See the README about tricky timezone issues!
-// I figured since this is i18n-friendly, we'd wanna
-// make sure the timezones were right :-)
-const christmasDate = new Date('2022/12/25')
 </script>
